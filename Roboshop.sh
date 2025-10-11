bbfc69189f8979339fd1d46f6e6e1380c4c0a24d
Dinesh velpula <velpuladinesh887@gmail.com>
2025-10-11 22:58:07 +0530

    committing

---
+++ Roboshop.sh
@@ -0,0 +1,16 @@
+#!binbash
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-0ada60f774a1d7eb8"
+
+for instance in $@
+do
+    instance_id=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0ada60f774a1d7eb8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=test }]" --query 'Instances[0].InstanceId' --output text)
+    if [ $instane != "frontend" ]; then
+       IP=$( aws ec2 describe-instances --instance-ids $instane_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text )
+    else
+        IP=$( aws ec2 describe-instances --instance-ids $instane_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text )
+    fi
+
+    echo ( $instane : $IP)
+
+done
