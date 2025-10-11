c18785dbacb6b92249e4e4cca728e5513eb4408e
Dinesh velpula <velpuladinesh887@gmail.com>
2025-10-11 23:14:25 +0530

    committing

--- Roboshop.sh
+++ Roboshop.sh
@@ -4,8 +4,7 @@ SG_ID="sg-0ada60f774a1d7eb8"
 
 for instance in $@
 do
-    instance_id=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID
-     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance }]" --query 'Instances[0].InstanceId' --output text)
+    instance_id=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance }]" --query 'Instances[0].InstanceId' --output text)
     if [ $instance != "frontend" ]; then
        IP=$( aws ec2 describe-instances --instance-ids $instane_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text )
     else
