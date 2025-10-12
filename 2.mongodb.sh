612a20b3461aa06ea0c7de7a3d934d46da7271a9
Dinesh velpula <velpuladinesh887@gmail.com>
2025-10-12 20:58:39 +0530

    committing

---
+++ 2.mongodb.sh
@@ -0,0 +1,35 @@
+userid=$( id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+folder="/var/log/mongodb-log"
+script=$( echo $0)
+log_file="$folder/$script.log"
+mkdir -p $folder
+echo "this scrip has started at : $(date)"
+if [ $userid -ne 0 ]; then
+    echo "ERROR please run the script with sudo privillages"
+    exit 1
+fi
+validate(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 is...... $R failed $N" | tee -a $log_file
+        exit 1
+    else
+        echo -e "$2 is......$G success $N" | tee -a $log_file
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+validate $? "adding mongo repo"
+
+dnf install mongodb-org -y 
+validate $? "ainstalling mongoDB"
+
+systemctl enable mongod 
+validate $? "enabling mongoDB"
+
+systemctl start mongod 
+validate $? "start mongoDB"
+
