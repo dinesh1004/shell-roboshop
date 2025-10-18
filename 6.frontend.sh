0c438161b4fc70de12698ff98d010792428790c7
Dinesh velpula <velpuladinesh887@gmail.com>
2025-10-18 19:14:26 +0530

    committing

--- 6.frontend.sh
+++ 6.frontend.sh
@@ -1,56 +1,110 @@
-userid=$( id -u)
+# userid=$( id -u)
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+# folder="/var/log/mongodb-log"
+# script=$( echo $0 | cut -d "." -f2 )
+# log_file="$folder/$script.log"
+# mkdir -p $folder
+# directory=$PWD
+# echo $directory
+# mongodb_host=mongodb.suneel.shop
+# script_dir=$PWD
+# echo "$script_dir"
+# echo "this scrip has started at : $(date)"
+
+# if [ $userid -ne 0 ]; then
+#     echo "ERROR please run the script with sudo privillages"
+#     exit 1
+# fi
+# validate(){
+#     if [ $1 -ne 0 ]; then
+#         echo -e "$2 is...... $R failed $N" | tee -a $log_file
+#         exit 1
+#     else
+#         echo -e "$2 is......$G success $N" | tee -a $log_file
+#     fi
+# }
+# dnf module disable nginx -y &>>log_file
+# dnf module enable nginx:1.24 -y &>>log_file
+# dnf install nginx -y &>>log_file
+# validate $? "installing nginx"
+
+# systemctl enable nginx &>>log_file
+# validate $? "enabling nginx"
+
+# systemctl start nginx 
+# validate $? "starting nginx"
+
+# rm -rf /usr/share/nginx/html/* 
+
+# validate $? "removing default content"
+# curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>log_file
+# validate $? "downloading application"
+
+# cd /usr/share/nginx/html 
+
+# unzip /tmp/frontend.zip &>>log_file
+# validate $? "unzipping the application"
+
+# rm -rf /etc/nginx/nginx.conf
+
+# cp $script_dir/nginx.conf /etc/nginx/nginx.conf
+# validate $? "copying  nginx.conf"
+
+# systemctl restart nginx 
+# validate $? "restarting  nginx"
+
+#!/bin/bash
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-folder="/var/log/mongodb-log"
-script=$( echo $0 | cut -d "." -f2 )
-log_file="$folder/$script.log"
-mkdir -p $folder
-directory=$PWD
-echo $directory
-mongodb_host=mongodb.suneel.shop
-script_dir=$PWD
-echo "$script_dir"
-echo "this scrip has started at : $(date)"
 
-if [ $userid -ne 0 ]; then
-    echo "ERROR please run the script with sudo privillages"
-    exit 1
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
-validate(){
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "$2 is...... $R failed $N" | tee -a $log_file
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 is......$G success $N" | tee -a $log_file
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
-dnf module disable nginx -y &>>log_file
-dnf module enable nginx:1.24 -y &>>log_file
-dnf install nginx -y &>>log_file
-validate $? "installing nginx"
 
-systemctl enable nginx &>>log_file
-validate $? "enabling nginx"
+dnf module disable nginx -y &>>$LOG_FILE
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx  &>>$LOG_FILE
 systemctl start nginx 
-validate $? "starting nginx"
+VALIDATE $? "Starting Nginx"
 
 rm -rf /usr/share/nginx/html/* 
-
-validate $? "removing default content"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>log_file
-validate $? "downloading application"
-
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 cd /usr/share/nginx/html 
-
-unzip /tmp/frontend.zip &>>log_file
-validate $? "unzipping the application"
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Downloading frontend"
 
 rm -rf /etc/nginx/nginx.conf
-
-cp $script_dir/nginx.conf /etc/nginx/nginx.conf
-validate $? "copying  nginx.conf"
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying nginx.conf"
 
 systemctl restart nginx 
-validate $? "restarting  nginx"
+VALIDATE $? "Restarting Nginx"
