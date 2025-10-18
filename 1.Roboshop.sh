cd2f6f2965311d5cf24ee0d0bbd8fa3582372d79
Dinesh velpula <velpuladinesh887@gmail.com>
2025-10-18 18:51:05 +0530

    committing

--- 1.Roboshop.sh
+++ 1.Roboshop.sh
@@ -5,7 +5,7 @@ dns_name="suneel.shop"
 
 for instance in $@
 do
-    instance_id=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance }]" --query 'Instances[0].InstanceId' --output text)
+    instance_id=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     if [ $instance != "frontend" ]; then
        IP=$( aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text )
        RECORD_NAME="$instance.$dns_name"
