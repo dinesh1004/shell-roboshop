0e56734bcf705e812c12dfa3278a3467fa4be9fe
Dinesh velpula <velpuladinesh887@gmail.com>
2025-10-21 10:05:58 +0530

    committing

--- test/1.mongodb.sh
+++ test/1.mongodb.sh
@@ -25,17 +25,17 @@ validate(){
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 validate $? "copying mongodb repo"
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>>log_file
 validate $? "installing mongodb"
 
-systemctl enable mongod 
+systemctl enable mongod &>>log_file
 validate $? "ienabling mongodb" 
 
-systemctl start mongod 
+systemctl start mongod &>>log_file
 validate $? "starting mongodb"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 validate $? "alloeing remote connections to mongodb"
 
-systemctl restart mongod
+systemctl restart mongod &>>log_file
 validate $? "restarting mongodb"
