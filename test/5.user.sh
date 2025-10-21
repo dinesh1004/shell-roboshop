56bb6e7467e5cfe943ddf59faca3c7551ccba883
Dinesh velpula <velpuladinesh887@gmail.com>
2025-10-21 15:25:21 +0530

    committing

--- test/5.user.sh
+++ test/5.user.sh
@@ -1,68 +1,142 @@
-#!binbash
-userid=$( id -u)
+# #!binbash
+# userid=$( id -u)
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+# folder="/var/log/mongodb-log"
+# script=$( echo $0)
+# log_file="$folder/$script.log"
+# mkdir -p $folder
+# script_dir=$PWD
+# echo "this scrip has started at : $(date)"
+# if [ $userid -ne 0 ]; then
+#     echo "ERROR please run the script with sudo privillages"
+#     exit 1
+# fi
+# validate(){
+#     if [ $1 -ne 0 ]; then
+#         echo -e "$2 is...... $R failed $N" | tee -a $log_file
+#         exit 1
+#     else
+#         echo -e "$2 is......$G success $N" | tee -a $log_file
+#     fi
+# }
+
+# dnf module disable nodejs -y &>>log_file
+# validate $? "disabling nodejs"
+# dnf module enable nodejs:20 -y &>>log_file
+# validate $? "enabling nodejs:20"
+# dnf install nodejs -y &>>log_file
+# validate $? "installing  nodejs"
+
+# id roboshop &>>log_file
+# if [ $? -ne 0 ]; then
+#     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+#     validate $? "adding roboshop user"
+# else 
+#     echo -e "roboshop user already exist......$Y SKIPPING $N"
+# fi 
+
+# mkdir -p /app 
+# validate $? "creating app directory"
+
+# rm -rf /app/*
+# validate $? "removing old data"
+
+# curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>log_file
+# validate $? "downloading th eapplication"
+# cd /app 
+# unzip /tmp/user.zip &>>log_file
+# validate $? "unzipping th eapplication"
+
+# cd /app 
+
+# npm install &>>log_file
+# validate $? "installing dependencies"
+
+# cp $script_dir/user.service /etc/systemd/system/user.service
+
+# systemctl daemon-reload
+
+# systemctl enable user &>>log_file
+# validate $? "enabling user"
+
+# systemctl start user &>>log_file
+# validate $? "restarting user"
+
+
+#!/bin/bash
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-folder="/var/log/mongodb-log"
-script=$( echo $0)
-log_file="$folder/$script.log"
-mkdir -p $folder
-script_dir=$PWD
-echo "this scrip has started at : $(date)"
-if [ $userid -ne 0 ]; then
-    echo "ERROR please run the script with sudo privillages"
-    exit 1
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
-validate(){
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-        echo -e "$2 is...... $R failed $N" | tee -a $log_file
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 is......$G success $N" | tee -a $log_file
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
-dnf module disable nodejs -y &>>log_file
-validate $? "disabling nodejs"
-dnf module enable nodejs:20 -y &>>log_file
-validate $? "enabling nodejs:20"
-dnf install nodejs -y &>>log_file
-validate $? "installing  nodejs"
+##### NodeJS ####
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y  &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
 
-id roboshop &>>log_file
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-    validate $? "adding roboshop user"
-else 
-    echo -e "roboshop user already exist......$Y SKIPPING $N"
-fi 
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "User already exist ... $Y SKIPPING $N"
+fi
 
-mkdir -p /app 
-validate $? "creating app directory"
+mkdir -p /app
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading user application"
+
+cd /app 
+VALIDATE $? "Changing to app directory"
 
 rm -rf /app/*
-validate $? "removing old data"
+VALIDATE $? "Removing existing code"
 
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>log_file
-validate $? "downloading th eapplication"
-cd /app 
-unzip /tmp/user.zip &>>log_file
-validate $? "unzipping th eapplication"
+unzip /tmp/user.zip &>>$LOG_FILE
+VALIDATE $? "unzip user"
 
-cd /app 
+npm install &>>$LOG_FILE
+VALIDATE $? "Install dependencies"
 
-npm install &>>log_file
-validate $? "installing dependencies"
-
-cp $script_dir/user.service /etc/systemd/system/user.service
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+VALIDATE $? "Copy systemctl service"
 
 systemctl daemon-reload
+systemctl enable user &>>$LOG_FILE
+VALIDATE $? "Enable user"
 
-systemctl enable user &>>log_file
-validate $? "enabling user"
-
-systemctl start user &>>log_file
-validate $? "restarting user"
-
-
+systemctl restart user
+VALIDATE $? "Restarted user"
